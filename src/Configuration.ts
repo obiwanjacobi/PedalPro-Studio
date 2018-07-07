ec8d3cd844d093677dbd9ba65d2ed0b626c87c4d
Marc Jacobi <Marc.Jacobi@macaw.nl>
2018-07-07 17:11:11 +0200

    moved texts json

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -2,5 +2,8 @@ export const Configuration = {
     pedalpro: {
         factoryFile: "./assets/PedalPro.65.vrf",
         factoryFileEx: "./assets/PedalProEx.81.vrf",
+    },
+    application: {
+        languageFile: "./assets/Texts.${lang}.json"
     }
 };
